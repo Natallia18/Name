aa98d92c3e67780b41a2b4be964eb3f86864a37b
Natallia18 <58847015+Natallia18@users.noreply.github.com>
2020-01-28 16:18:12 +0300

    My tasks

--- inheritance.js
+++ inheritance.js
@@ -1,23 +1,17 @@
 class Dog{
-
     setName(name) {
       this.name = name;
-  
     }
-  
     startRunning(){
       console.log ("The dog " + this.name + " started running");
-    
     }
     stopBarking(){
       console.log ("The dog " + this.name + " stopped barking");
     }
   }
   class Beagle extends Dog{
-  
-    topSpeed(speed){
-      console.log ("Top speed for "+this.name+" is "+speed);
-  
+  topSpeed(speed){
+  console.log ("Top speed for "+this.name+" is "+speed);
     }
   }
   let myDog = new Beagle();
