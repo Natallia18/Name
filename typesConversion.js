88dda54493879d2f438cb7ce5fc5c1e91f52dcf6
Natallia18 <58847015+Natallia18@users.noreply.github.com>
2020-01-28 16:21:23 +0300

    My tasks

--- typesConversion.js
+++ typesConversion.js
@@ -2,36 +2,25 @@
 let a = 1; 
 x = a + a;
 console.log(x);
-
-
 // subtraction
-let a = 1; 
 x = a - a;
 console.log(x);
-
 // multiplication
-let a = 1; 
 x = a * a;
 console.log(x);
-
 // division
-let a = 1; 
 x = a / a;
 console.log(x);
-
 // Number to String
-value = String(10);
-value = String(10+40);
+value = 40;
 value = (40).toString();
-alert (value);
-
+console.log(value);
 // Boolean to String
 value = String(false);
-alert(value);
-
+console.log(value);
 // Array to String
 value = String([1,2,3]);
-alert(value);
+console.log(value);
 
 
 
