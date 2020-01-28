8f150e15b0e70e25b6ff093de40c28b87ecac221
Natallia18 <58847015+Natallia18@users.noreply.github.com>
2020-01-28 16:15:23 +0300

    My tasks

--- encapsulation.js
+++ encapsulation.js
@@ -1,19 +1,15 @@
 class Person{
-
     setPersDetails(name,age) {
-      this.name = name;
-      this.age = age;
-  
+        this.name = name;
+        this.age = age;
     }
     getPersName() {
       return this.name;
     }
     getPersAge() {
       return this.age;
-  
     }
   }
-  
   let pers1 = new Person();
   pers1.setPersDetails("Galina, 37");
   console.log (pers1.getPersName());
