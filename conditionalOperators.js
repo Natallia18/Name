c67aa220142151af3316c11ddac4790981cdd306
Natallia18 <58847015+Natallia18@users.noreply.github.com>
2020-01-28 16:12:21 +0300

    My tasks

---
+++ conditionalOperators.js
@@ -0,0 +1,31 @@
+// if
+let age = 20;
+if (age > 18) {
+    console.log("Большиш");
+} else if (age <  18) {
+    console.log("Не самый взрослый");
+}
+// ?
+let a =2;
+console.log(a > 3? 'Красивый' : 'Умный');
+// comparison
+let value;
+value = 1 > 2;
+console.log(value);
+value = 1 == 2;
+console.log(value);
+value = 1 != 2;
+console.log(value);
+value = ( 'A' > 'z' );
+console.log(value);
+value = ("Кот" > "КОд");
+console.log(value);
+value = ("Ананас" < "ананас");
+console.log(value);
+value = ( '22' > '111' );
+console.log(value);
+console.log( true == 1 );
+console.log( false == 0 );
+
+
+
