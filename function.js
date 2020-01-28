db3d5867d9180814d06e2f52d9bcd1f5d3f3f678
Natallia18 <58847015+Natallia18@users.noreply.github.com>
2020-01-28 16:17:29 +0300

    My tasks

--- function.js
+++ function.js
@@ -1,55 +1,43 @@
 // digits addition
-
-function summa(a,b) {
-    return (a+b);
+function sum(a,b) {
+    return (a + b);
 }
-alert(summa (4,5) );
-
+console.log(sum (4,5));
 // name + Hello
 var userName = 'Ната';
-
-function showMessage(){
+function user(){
     userName = 'Ната';
     var message = 'Привет,' + userName;
-    alert(message);
+    console.log(message);
 }
-showMessage();
-
+user();
 // type of argument
-function detect_data_type(value)
+function data_type(value)
 {
-var dtypes = [Function, RegExp, Number, String, Boolean, Object], x, len;
-    
-if (typeof value === "object" || typeof value === "function") 
-    {
-     for (x = 0, len = dtypes.length; x < len; x++) 
-     {
-            if (value instanceof dtypes[x])
-            {
-                return dtypes[x];
+var dtypes = [Function, RegExp, Number, String, Boolean, Object], num, len;
+if (typeof value === "object" || typeof value === "function") {
+     for (num = 0, len = dtypes.length; num < len; num++) {
+         if (value instanceof dtypes[x]) {
+             return dtypes[num];
             }
       }
     }
-    
     return typeof value;
 }
-console.log(detect_data_type(12));
-console.log(detect_data_type('w3resource'));
-console.log(detect_data_type(false));
-
-
+console.log(data_type(12));
+console.log(data_type('w3resource'));
+console.log(data_type(false));
 // even elements of array
 let help = [20, 16, 14, 5, 3];
 function showMessage(array) {
 let arr = [];
 for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 == 0) {
-        arr.push(array[i])
-      
+        arr.push(array[i]) 
     }
   }
   return arr
 }
-alert(showMessage(help));
+console.log(showMessage(help));
 
 
